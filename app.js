70c9c5b2d41764a5acfff9222b5361d23b67d8c2
ElkFly <elkfly@qq.com>
2017-03-28 01:59:26 +0800

    一些移动段兼容性

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ angular.module('elkfly', [
 ]).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/home'});
 }]);
+
+
+function getIE() {
+    if (window.ActiveXObject) {
+        var v = navigator.userAgent.match(/MSIE ([^;]+)/)[1];
+        return parseFloat(v.substring(0, v.indexOf(".")))
+    }
+    return false
+}
+if (getIE()) {
+    // window.location = window.location.href + '404.html'
+    window.location = window.location.pathname = '/MyApp/404.html'
+}
--- js/main.js
+++ js/main.js
@@ -394,17 +394,11 @@
     }
 
     function getIE() {
-
         if (window.ActiveXObject) {
-
             var v = navigator.userAgent.match(/MSIE ([^;]+)/)[1];
-
             return parseFloat(v.substring(0, v.indexOf(".")))
-
         }
-
         return false
-
     }
 
     // event.pageX
